1c95efaad1b27646b6f7ce374ae14d2948c80f9b
roymnel-rgb <roymnel@gmail.com>
2026-08-16 07:51:57 -0400

    Update Type.kt

--- app/src/main/java/com/example/dora_hud/ui/theme/Type.kt
+++ app/src/main/java/com/example/dora_hud/ui/theme/Type.kt
@@ -1,1 +1,7 @@
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.dora_hud.R
 
+val Digital7 = FontFamily(
+    Font(R.font.digital_7_italic)
+)
